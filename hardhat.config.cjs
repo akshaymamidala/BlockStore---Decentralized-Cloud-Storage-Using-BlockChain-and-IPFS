9365d4c5a75e46164635bb3cb9ccb53bbff27b4d
akshaymamidala <akshakmah30@gmail.com>
2026-03-08 16:13:12 +0530

    Add files via upload

---
+++ hardhat.config.cjs
@@ -0,0 +1,15 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+const sepoliaRpcUrl = process.env.SEPOLIA_RPC_URL || "";
+const sepoliaPrivateKey = process.env.SEPOLIA_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: sepoliaRpcUrl,
+      accounts: sepoliaPrivateKey ? [sepoliaPrivateKey] : [],
+    },
+  },
+};
